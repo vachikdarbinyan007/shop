8865fa060e5ebda5d21e259a87f6be833466971a
vachikdarbinyan007 <vachikdarbinyan007@gmail.com>
2023-03-14 15:21:30 +0100

    first comit

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>YANIAN | Home</title>
+	<link rel="stylesheet" type="text/css" href="assets/css/style.css">
+</head>
+<body>
+	<header>
+		<nav>
+			<ul>
+				<li><a href="">Home</a></li>
+				<li><a href="">About us</a></li>
+				<li><a href="">Products</a></li>
+				<li>
+					<form action="" method="post"><input placeholder="Search for Products..." id="search_input" name="search" type="search"/><input type="submit" value="Search" id="search_btn"></form>
+				</li>
+			</ul>
+		</nav>
+	</header>
+</body>
+</html>
